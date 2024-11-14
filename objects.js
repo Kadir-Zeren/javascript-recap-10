d5411f7173d070e44110cbdd3d8ccb92f20f68d4
kadir-zeren <k4dzeren@gmail.com>
2024-11-14 09:47:41 +0300

    squaere breacket

---
+++ objects.js
@@ -0,0 +1,59 @@
+//* ===========================================
+//*                 (OBJECTS)
+//* ===========================================
+
+//? Diziler sirali bellek bolgeleridir.Bu yuzden dizilere
+//? indeksleme ile erisebiliriz. Ancak daha karmasik (unstructured)
+//? verilerimiz icin Object'leri kullanabiliriz.
+
+//? Object'lerde Key_value (property-value) yapisi kullanilir.
+//? Herhangi bir veriyi erismek icin property (key) adi kullanilir.
+
+console.log(" ******** Objects ********* ");
+
+//* -----------------------------------------------
+//* 1- Object literal (En cok tercih edilen yontem)
+//* -----------------------------------------------
+
+const car = {
+  brand: "Mercedes",
+  model: 2020,
+  engine: 2000,
+  speed: 5,
+  color: ["pink", "black", "blue", "green"],
+};
+console.log(car);
+
+//? Objenin okunması (Read)
+
+console.log(car.brand); //* . notasyonu ile erişim.
+console.log(car.color[0]); //? pink
+
+console.log(car.color);
+car.color.forEach((c) => console.log(c));
+
+console.log(car.Engine); //? undefined
+
+//? Objenin degerinin degisitirilmesi (write)
+car.speed = 7;
+car.color = "Morcivert";
+car.class = "E";
+
+console.log(car);
+
+//! Uncaught TypeError: Assignment to constant variable.
+// car = {
+//   deneme: "1",
+// };
+
+//* Square Bracket yontemi ile erisim
+//! bu yontem key isimlerinin degiskenlerde saklanmasına izin verdigi icin dinamik olarak
+// obje erisimlerinde kullanılabilir
+
+console.log("ENGINE:", car["engine"]); //? objeAdi["keyAdi"]
+
+const myKey = prompt(
+  "Araba ile ilgili Neyi ogrenmek istersiniz: speed, color, engine ... "
+);
+
+alert(car[myKey]);
